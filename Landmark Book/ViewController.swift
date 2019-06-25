41e3ed29fb7920cbb59cdc68a85bd83bac077855
İzzet Kara <izzet26kara@gmail.com>
2019-06-25 06:23:45 +0300

    Update: Canakkale added. Delete Rows added.

--- Landmark Book/ViewController.swift
+++ Landmark Book/ViewController.swift
@@ -24,6 +24,15 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+       // Bir satırı kullanıcı kendi silmek isterse bu fonksiyonu kullanablirim.<
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            landmarkNames.remove(at: indexPath.row)
+            landmarkImage.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        }
+    }
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -37,12 +46,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         landmarkNames.append("Eskisehir")
         landmarkNames.append("Antalya")
         landmarkNames.append("Izmir")
+        landmarkNames.append("Canakkale")
         
         landmarkImage.append(UIImage(named: "eskisehir.jpg")!)
         landmarkImage.append(UIImage(named: "istanbul.jpg")!)
         landmarkImage.append(UIImage(named: "antalya.jpg")!)
         landmarkImage.append(UIImage(named: "izmir.jpg")!)
-        
+        landmarkImage.append(UIImage(named: "canakkale.jpg")!)
         
         
     }
